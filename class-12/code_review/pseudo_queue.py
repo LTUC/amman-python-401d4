06248fa6a426a986ae8479e9acd55af4fae664a4
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2021-06-21 12:29:13 +0300

    Added demo for class 12 and code review for class 11

---
+++ class-12/code_review/pseudo_queue.py
@@ -0,0 +1,44 @@
+"""
+- PseudoQueue class
+- enqueue and dequeue
+- 2 stacks to implement
+"""
+
+"""
+
+s1 = Satck()
+s2 = Stack()
+
+func enqueue(value):
+    # just push to s1
+    # s2 is empty
+
+
+enqueue(4)
+s1 = [4]
+s2 = []
+
+enqueue(7)
+s1 = [4,7]
+s2 = []
+
+enqueue('r')
+s1 = [4,7,'r']
+s2 = []
+
+
+Satck.push(value)
+Stack.pop()
+
+func dqueue():
+    s1.pop(), s2.push('r')
+    s1.pop(), s2.push(7)
+    s2 = ['r', 7], s1 = []
+    s1.pop() # your target 4, return it
+    s2.pop() -> s1.push()
+    s2.pop() -> s1.push()
+    s2 = []
+    s1 = [7,'r']
+
+
+"""
